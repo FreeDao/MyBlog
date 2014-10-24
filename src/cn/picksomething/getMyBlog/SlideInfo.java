9c1dd77893a08669d67ef9c66a7480d56038aa0a
picksomething <zzucaobin@163.com>
2014-10-24 09:22:36 +0800

    modify slidemenu

--- src/cn/picksomething/getMyBlog/SlideInfo.java
+++ src/cn/picksomething/getMyBlog/SlideInfo.java
@@ -1,10 +1,8 @@
 package cn.picksomething.getMyBlog;
 
-import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
-
-import cn.picksomething.getmyblog.R;
 import android.app.Activity;
 import android.os.Bundle;
+import cn.picksomething.getmyblog.R;
 
 public class SlideInfo extends Activity{
 	
@@ -14,17 +12,6 @@ public class SlideInfo extends Activity{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.slide_info_layout);
 		
-		/*** 初始化侧滑菜单 Begin ***/
-        SlidingMenu menu = new SlidingMenu( this);
-        menu.setMode(SlidingMenu. LEFT);
-        menu.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN );
-        menu.setShadowWidthRes(R.dimen. shadow_width);        // 1）
-        menu.setShadowDrawable(R.drawable. shadow);           // 2）
-        menu.setBehindOffsetRes(R.dimen.slidingmenu_offset ); // 3）
-        menu.setFadeDegree(0.35f);
-        menu.attachToActivity( this, SlidingMenu.SLIDING_CONTENT );
-        menu.setMenu(R.layout.activity_main); // 4）
-        /*** 初始化侧滑菜单 End ***/
 	}
 
 }
