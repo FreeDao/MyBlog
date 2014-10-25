589a3905dfa5cc9af862e8d9d21dde0a8b7ccd29
picksomething <zzucaobin@163.com>
2014-10-25 23:18:58 +0800

    perfect webview

--- src/cn/picksomething/getMyBlog/MyWebView.java
+++ src/cn/picksomething/getMyBlog/MyWebView.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Window;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import cn.picksomething.getmyblog.R;
@@ -18,27 +20,48 @@ public class MyWebView extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
+		this.getWindow().requestFeature(Window.FEATURE_PROGRESS);
 		setContentView(R.layout.webview_layout);
 		myWebView = (WebView) findViewById(R.id.webview);
 		myWebView.setWebViewClient(new MyWebViewClient());
+		myWebView.setWebChromeClient(new MyWebChromeClient());
 		intent = getIntent();
 		String url = intent.getStringExtra("url");
 		Log.d("caobin", url);
 		myWebView.loadUrl(url);
 	}
-	
+
 	private class MyWebViewClient extends WebViewClient {
-	    @Override
-	    public boolean shouldOverrideUrlLoading(WebView view, String url) {
-	        if (Uri.parse(url).getHost().equals("www.picksomething.cn")) {
-	            // This is my web site, so do not override; let my WebView load the page
-	            return false;
-	        }
-	        // Otherwise, the link is not for a page on my site, so launch another Activity that handles URLs
-	        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
-	        startActivity(intent);
-	        return true;
-	    }
+		@Override
+		public boolean shouldOverrideUrlLoading(WebView view, String url) {
+			if (Uri.parse(url).getHost().equals("www.picksomething.cn")) {
+				// This is my web site, so do not override; let my WebView load
+				// the page
+				return false;
+			}
+			// Otherwise, the link is not for a page on my site, so launch
+			// another Activity that handles URLs
+			Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+			startActivity(intent);
+			return true;
+		}
+	}
+
+	private class MyWebChromeClient extends WebChromeClient {
+		@Override
+		public void onProgressChanged(WebView view, int newProgress) {
+			// TODO Auto-generated method stub
+			super.onProgressChanged(view, newProgress);
+			MyWebView.this.setProgress(newProgress * 100);
+			Log.d("caobin", "ProgressChanged, new Progress = " + newProgress);
+		}
+		
+		@Override
+		public void onReceivedTitle(WebView view, String title) {
+			// TODO Auto-generated method stub
+			super.onReceivedTitle(view, title);
+			MyWebView.this.setTitle(title);
+		}
 	}
 
 }
