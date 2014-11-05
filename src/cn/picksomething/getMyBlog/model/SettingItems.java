988b461751466c1839ad0a8ec9c0f8405b7b5674
picksomething <zzucaobin@163.com>
2014-11-05 14:28:37 +0800

    add onclick listener for slidemenu options

--- src/cn/picksomething/getMyBlog/model/SettingItems.java
+++ src/cn/picksomething/getMyBlog/model/SettingItems.java
@@ -1,31 +1,41 @@
 package cn.picksomething.getMyBlog.model;
 
 public class SettingItems {
-	
+
 	private String itemName;
-	
+	private String itemURL;
+
 	public SettingItems() {
 		// TODO Auto-generated constructor stub
 		super();
 	}
-	
-	public SettingItems(String itemName){
+
+	public SettingItems(String itemName, String itemURL) {
 		super();
 		this.itemName = itemName;
+		this.itemURL = itemURL;
 	}
-	
-	public void setSettingItemsName(String itemName){
+
+	public void setSettingItemsName(String itemName) {
 		this.itemName = itemName;
 	}
-	
-	public String getSettingItemsName(){
+
+	public String getSettingItemsName() {
 		return itemName;
 	}
-	
+
+	public void setSettingItemsURL(String URL) {
+		this.itemURL = URL;
+	}
+
+	public String getSettingItemsURL() {
+		return itemURL;
+	}
+
 	@Override
 	public String toString() {
 		// TODO Auto-generated method stub
-		return "SettingItems[itemName=" + itemName + "]";
+		return "SettingItems[itemName=" + itemName + "itemURL=" + itemURL+ "]";
 	}
 
 }
--- src/cn/picksomething/getMyBlog/ui/CustomMenu.java
+++ src/cn/picksomething/getMyBlog/ui/CustomMenu.java
@@ -5,11 +5,17 @@ import java.util.List;
 
 import android.app.Fragment;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import cn.picksomething.getMyBlog.MyWebView;
 import cn.picksomething.getMyBlog.adapter.SlideMenuAdapter;
 import cn.picksomething.getMyBlog.model.SettingItems;
 import cn.picksomething.getmyblog.R;
@@ -19,10 +25,12 @@ public class CustomMenu extends Fragment {
 	private Context mContext = null;
 	private ListView sortListView = null;
 	private ListView functionListView = null;
+	private LinearLayout mInfo = null;
 	private List<SettingItems> mSorts = null;
 	private List<SettingItems> mFunctions = null;
 	private SlideMenuAdapter sortAdapter = null;
 	private SlideMenuAdapter functionAdapter = null;
+	private final static String myurl = "http://www.picksomething.cn/?page_id=2";
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -45,6 +53,7 @@ public class CustomMenu extends Fragment {
 		// TODO Auto-generated method stub
 		sortListView = (ListView) mView.findViewById(R.id.listview_sort);
 		functionListView = (ListView) mView.findViewById(R.id.listview_function);
+		mInfo = (LinearLayout)mView.findViewById(R.id.person_info);
 
 	}
 
@@ -55,17 +64,22 @@ public class CustomMenu extends Fragment {
 	 */
 	private void initDate() {
 		// TODO Auto-generated method stub
+		mInfo.setFocusable(true);
+		mInfo.setClickable(true);
 		mContext = mView.getContext();
 		mSorts = new ArrayList<SettingItems>();
 		mFunctions = new ArrayList<SettingItems>();
 		String[] sortsArray = mContext.getResources().getStringArray(R.array.sorts);
+		String[] sortsUrlArray = mContext.getResources().getStringArray(R.array.sorts_url);
 		String[] functionsArray = mContext.getResources().getStringArray(R.array.functions);
+		String[] funStringsUrlArray = mContext.getResources().getStringArray(R.array.functions_url);
+		
 		for (int i = 0; i < sortsArray.length; i++) {
-			SettingItems sortsItems = new SettingItems(sortsArray[i]);
+			SettingItems sortsItems = new SettingItems(sortsArray[i], sortsUrlArray[i]);
 			mSorts.add(sortsItems);
 		}
 		for (int j = 0; j < functionsArray.length; j++) {
-			SettingItems functionsItems = new SettingItems(functionsArray[j]);
+			SettingItems functionsItems = new SettingItems(functionsArray[j], funStringsUrlArray[j]);
 			mFunctions.add(functionsItems);
 		}
 	}
@@ -77,12 +91,32 @@ public class CustomMenu extends Fragment {
 	 */
 	private void bindDate() {
 		// TODO Auto-generated method stub
+		mInfo.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent intent = new Intent(mContext, MyWebView.class);
+				intent.putExtra("url", myurl);
+				startActivity(intent);
+			}
+		});
 		sortAdapter = new SlideMenuAdapter(mContext, mSorts);
 		functionAdapter = new SlideMenuAdapter(mContext, mFunctions);
 		sortListView.setAdapter(sortAdapter);
 		functionListView.setAdapter(functionAdapter);
-		sortAdapter.notifyDataSetChanged();
-		functionAdapter.notifyDataSetChanged();
+		sortListView.setOnItemClickListener(new OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+				// TODO Auto-generated method stub
+				SettingItems items = mSorts.get(position);
+				String url = items.getSettingItemsURL();
+				Intent intent = new Intent(mContext, MyWebView.class);
+				intent.putExtra("url", url);
+				startActivity(intent);
+			}
+		});
 	}
 
 }
